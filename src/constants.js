62b7ad5e536800e6195cffb274ed96095dff5adb
Ali Kim <alikim@gmail.com>
2021-10-25 18:13:20 -0400

    first commit

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+const CONTRACT_ADDRESS = '0x8ACd6c3CDd1497373E6ceE0784E9BC46DFf3054D';
+
+
+const transformCharacterData = (characterData) => {
+    return {
+        name: characterData.name,
+        imageURI: characterData.imageURI,
+        hp: characterData.hp.toNumber(),
+        maxHp: characterData.maxHp.toNumber(),
+        attackDamage: characterData.attackDamage.toNumber(),
+    };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
